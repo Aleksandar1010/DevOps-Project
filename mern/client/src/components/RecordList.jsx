6ea5185050228007107f42d6132966df3a88882b
Aleksandar1010 <aleksandar.sajkoski@students.finki.ukim.mk>
2024-09-23 00:59:39 +0200

    Update frontend test

--- mern/client/src/components/RecordList.jsx
+++ mern/client/src/components/RecordList.jsx
@@ -100,7 +100,7 @@ export default function RecordList() {
                   Action
                 </th>
                 <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                  Test Change1234
+                  Test Change1233
                 </th>
               </tr>
             </thead>
